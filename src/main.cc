42e8512e2142eb42794949e64661ae8be7b285bc
dntoll <daniel@familjentoll.se>
2014-11-30 10:29:14 +0100

    more demo

--- src/main.cc
+++ src/main.cc
@@ -12,12 +12,13 @@ int main()
 	for (int i= 0;i< 5; i++) {
 		cout << "Write Some pixels \n";	
 		inst.clearScreen();
-		sleep(1);
+		
 	
 		for (int i = 0; i < 5; i++) {
 			inst.setColor(rand()%240, rand()%320, rand()%255, rand()%255, rand()%255);
 			inst.fillBox(rand()%240, rand()%320, 50, 50, rand()%255, rand()%255, rand()%255);	
 		}
+		sleep(1);
 	}
 	
 	inst.setBrightness(255);
