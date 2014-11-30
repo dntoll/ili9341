1036628774d9a09c9d9cecb0f1d96c1da5f10414
dntoll <daniel@familjentoll.se>
2014-11-30 09:23:46 +0100

    write random pixels

--- src/ili9341.cc
+++ src/ili9341.cc
@@ -130,7 +130,7 @@ void ili9341::setColor(int x, int y, int r, int g, int b)
 	int bch=((r&248)|g>>5);
 	int bcl=((g&28)<<3|b>>3);
 	int color = (bch<<8) | bcl;
-	Address_set(x, y, x+10, y+10);	
+	Address_set(x, y, x+1, y+1);	
 	for (int i = 0; i< 10; i++) {
 		LCD_Write_DATA(bch);
 		LCD_Write_DATA(bcl);
