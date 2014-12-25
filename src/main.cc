6a7a3b081d725fe0230ec81101c145c5b38f3469
dntoll <daniel@familjentoll.se>
2014-12-25 15:51:28 +0100

    double buffer start

--- src/main.cc
+++ src/main.cc
@@ -23,7 +23,7 @@ int main()
 		inst.clearScreen();
 
 		inst.fillBox(0, 0, 50, 50, 255, 0, 0);
-		inst.fillBox(240-50, 0, 50, 50, 0, 255, 0);
+		inst.fillBox(250-50, 0, 50, 50, 0, 255, 0);
 		inst.fillBox(120, 120, 50, 50, 0, 0, 255);
 	
 
