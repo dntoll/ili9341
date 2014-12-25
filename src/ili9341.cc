147127990a5bacc2cbee13a9deee7fbd154c9294
dntoll <daniel@familjentoll.se>
2014-12-25 17:57:49 +0100

    dirty

--- src/ili9341.cc
+++ src/ili9341.cc
@@ -109,7 +109,7 @@ void ili9341::clearScreen() {
 }
 
 void ili9341::flush() {
-	writeToBuffer(0, 0, WIDTH, HEIGHT);
+	//writeToBuffer(0, 0, WIDTH, HEIGHT);
 
 	for(int i=0; i < dirtyRects.size(); i++) {
 		writeToBuffer(dirtyRects[i].x,
