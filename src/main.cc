9885cf4c68d42a613f501f836224170316a0cc88
dntoll <daniel@familjentoll.se>
2014-12-25 14:30:37 +0100

    double buffer start

--- src/main.cc
+++ src/main.cc
@@ -17,7 +17,7 @@ int main()
 	inst.clearScreen();
 	inst.flush();
 	
-	cout << "Before \n";
+	/*cout << "Before \n";
 	for (int i= 0;i< 5; i++) {
 		cout << "Write Some pixels \n";
 		//inst.clearScreen();
@@ -26,16 +26,12 @@ int main()
 		//inst.fillBox(190, 0, 50, 50, 0, 255, 0);
 		//inst.fillBox(0, 120, 50, 50, 0, 0, 255);
 	
-		/*for (int i = 0; i < 5; i++) {
-			//inst.setColor(rand()%240, rand()%320, rand()%255, rand()%255, rand()%255);
-			inst.fillBox(rand()%240, rand()%320, 50, 50, 255, 255, 255);
 
-		}*/
 		cout << "Wrote Some pixels \n";
 		inst.flush();
 		sleep(1);
 		cout << "Flushed Some pixels \n";
-	}
+	}*/
 	
 	inst.setBrightness(0);
 	return 0;
