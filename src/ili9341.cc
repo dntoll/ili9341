3fe60104e6f8ac3fab7c1682f5d159a0464b04d5
dntoll <daniel@familjentoll.se>
2014-12-25 15:31:02 +0100

    double buffer start

--- src/ili9341.cc
+++ src/ili9341.cc
@@ -131,7 +131,7 @@ void ili9341::flush() {
 }
 
 int ili9341::toIndex(int x, int y) {
-	return y + x * HEIGHT;
+	return y + x * WIDTH;
 }
 
 void ili9341::writeToBuffer(int x, int y, int width, int height) {
