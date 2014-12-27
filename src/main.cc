8cf864d900ca079340da1e1b71429e09300401a4
dntoll <daniel@familjentoll.se>
2014-12-27 18:15:30 +0100

    debugging

--- src/main.cc
+++ src/main.cc
@@ -27,14 +27,14 @@ int main()
 
 
 	//inst.test();
-	
+	inst.fillBox(Rect(lcd.WIDTH/2-5, lcd.HEIGHT/2-5, 30, 12),Color::WHITE);
 
 	inst.fillBox(Rect(0, 0, 25, 50), Color(255, 	0, 		0));
 	inst.fillBox(Rect(lcd.WIDTH-25, 0, 25, 50), 	Color(0, 		255, 	0));
 	inst.fillBox(Rect(0, lcd.HEIGHT-50, 25, 50), 	Color(0, 		0, 		255));
 	inst.fillBox(Rect(lcd.WIDTH-25, lcd.HEIGHT-50, 25, 50),Color(255, 	0, 		255));
 
-	inst.fillBox(Rect(lcd.WIDTH/2-5, lcd.HEIGHT/2-5, 30, 12),Color::WHITE);
+
 
 	inst.fillBox(Rect(-25, lcd.HEIGHT/2-25, 50, 50), Color::WHITE);
 	inst.fillBox(Rect(lcd.WIDTH-25, lcd.HEIGHT/2-25, 50, 50), Color::WHITE);
